b64f6497e8ea408b02d22a3d112eb2847896a418
Nazara <128611399+dcbott01@users.noreply.github.com>
2024-07-31 17:43:36 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-import requests
-import time
-from colorama import Fore, Style, init
-from datetime import datetime
+import os
 import json
+import time
 import random
+import requests
+from datetime import datetime
+from colorama import init, Fore, Style
 
 # Initialize colorama
 init(autoreset=True)
@@ -13,6 +14,8 @@ get_url = "https://ranch-api.kuroro.com/api/Upgrades/GetPurchasableUpgrades"
 buy_url = "https://ranch-api.kuroro.com/api/Upgrades/BuyUpgrade"
 checkin_url = "https://ranch-api.kuroro.com/api/DailyStreak/ClaimDailyBonus"
 mining_url = "https://ranch-api.kuroro.com/api/Clicks/MiningAndFeeding"
+ball_url = "https://ranch-api.kuroro.com/api/EnergyBalls/TakeHitsCombo/{owner_id}:20240731"
+reff_url = "https://ranch-api.kuroro.com/api/Referrals/GetReferralsState"
 
 def read_bearer_tokens(file_path):
     with open(file_path, 'r') as file:
@@ -39,9 +42,9 @@ def perform_action(url, action_name, payload, bearer_token):
     try:
         response = requests.post(url, headers=headers, json=payload)
         if response.status_code == 200:
-            print(Fore.GREEN + f"Successfully performed {action_name}")
+            print(Fore.GREEN + f"{action_name} Success")
         else:
-            print(Fore.RED + f"Failed to perform {action_name} - Udah Abis Jir")
+            print(Fore.RED + f"{action_name} - Already Tap")
     except requests.RequestException as e:
         print(Fore.RED + f"An error occurred during {action_name}: {e}")
 
@@ -52,14 +55,56 @@ def checkin(bearer_token):
     }
     perform_action(checkin_url, f"Check-in for {current_date}", payload, bearer_token)
 
+def reffstate(bearer_token):
+    headers = create_headers(bearer_token)
+    try:
+        response = requests.get(reff_url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            owner_id = data.get("ownerId")
+            if owner_id:
+                return owner_id  
+            else:
+                print(Fore.RED + "Owner ID not found in response.")
+                return None
+        else:
+            print(Fore.RED + f"Failed to retrieve referral state - Status Code: {response.status_code}")
+    except requests.RequestException as e:
+        print(Fore.RED + f"An error occurred during reffstate: {e}")
+    return None
+
+def update_coin(bearer_token):
+    headers = create_headers(bearer_token)
+    try:
+        response = requests.post("https://ranch-api.kuroro.com/api/Game/UpdateCoinsSnapshot", headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            coins_update = data.get('value')
+            print(Fore.GREEN + f"[Balance] : {Fore.MAGENTA}{coins_update}{Style.RESET_ALL}")
+        else:
+            print(f"Error: Received status code {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def earnrate(bearer_token):
+    headers = create_headers(bearer_token)
+    try:
+        response = requests.get("https://ranch-api.kuroro.com/api/Game/GetPlayerState", headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            coins_earning_rate = data.get('coinsEarningRate')
+            print(Fore.GREEN + f"[Coins/Hours] : {Fore.MAGENTA}{coins_earning_rate}{Style.RESET_ALL}")
+        else:
+            print(f"Error: Received status code {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 def upgrade_process(bearer_token):
     headers = create_headers(bearer_token)
     response = requests.get(get_url, headers=headers)
 
     if response.status_code == 200:
         upgrades = response.json()
-
-        # Step 4: Purchase an upgrade
         for upgrade in upgrades:
             if upgrade['canBePurchased']:
                 payload = {
@@ -67,9 +112,9 @@ def upgrade_process(bearer_token):
                 }
                 purchase_response = requests.post(buy_url, json=payload, headers=headers)
                 if purchase_response.status_code == 200:
-                    print(Fore.GREEN + f"Successfully purchased upgrade: {upgrade['name']}")
+                    print(Fore.GREEN + f"Buy {upgrade['name']}")
                 else:
-                    print(Fore.RED + f"Failed to purchase upgrade: {upgrade['name']} - Duit lu Ga cukup bre $$$")
+                    print(Fore.RED + f"Not enough balance to buy {upgrade['name']} ")
     else:
         print(Fore.RED + f"Request failed with status code {response.status_code}")
 
@@ -89,48 +134,43 @@ def print_welcome_message():
     print(Fore.RED + Style.BRIGHT + "Update Link: https://github.com/dcbott01/kuroro")
     print(Fore.BLUE + Style.BRIGHT + "Tukang Rename MATI AJA")
 
-def process_accounts(user_choice):
+def main():
+    print_welcome_message()
+    user_choice = input("Do you want to proceed with upgrades for all accounts? (yes/no): ").strip().lower()
+
     bearer_tokens = read_bearer_tokens('query.txt')
-    
+    if not bearer_tokens:
+        print(Fore.RED + "No bearer tokens found. Exiting...")
+        return
+
     for i, bearer_token in enumerate(bearer_tokens, start=1):
         print(Fore.BLUE + Style.BRIGHT + f"\n========== PROCESSING ACCOUNT {i} ==========")
-        
-        # Step 1: Check-in for the daily bonus
         checkin(bearer_token)
-        
-        # Step 2: Perform Mining and Feeding
+        reffstate(bearer_token)
+        update_coin(bearer_token)
+        earnrate(bearer_token)
         mining_payload = {"mineAmount": 100, "feedAmount": 0}
         perform_action(mining_url, "Mining", mining_payload, bearer_token)
-        
-        feeding_payload = {"mineAmount": 0, "feedAmount": 1000}
+        feeding_payload = {"mineAmount": 0, "feedAmount": 10}
         perform_action(mining_url, "Feeding", feeding_payload, bearer_token)
-
-        # Perform upgrade process based on user choice
+        ball_payload = {"hits": 10}
+        perform_action(ball_url, "Tap Ball", ball_payload, bearer_token)
+        print(Fore.CYAN + Style.BRIGHT + f"\n========== PROCESSING UPGRADE ==========")
         if user_choice == 'yes':
             upgrade_process(bearer_token)
         else:
             print(Fore.YELLOW + "Skipping upgrade process for this account.")
-        
-        # Print message indicating transition to the next account
         print(Fore.CYAN + Style.BRIGHT + f"\nFinished processing account {i}. Moving to the next account...\n")
         time.sleep(5)
-    
-    # Print completion message and start countdown
+
     print(Fore.BLUE + Style.BRIGHT + f"\n========== SEMUA AKUN TELAH DI PROSES ==========")
-    for _ in range(1800):
-        minutes, seconds = divmod(1800 - _, 60)
+    for _ in range(900):
+        minutes, seconds = divmod(900 - _, 60)
         print(f"{random.choice([Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE])+Style.BRIGHT}==== [ Looping berikutnya {minutes} menit {seconds} detik ] ===={Style.RESET_ALL}", end="\r", flush=True)
         time.sleep(1)
 
-def main():
-    print_welcome_message()  # Print the welcome message at the start
-    
-    # Ask user if they want to proceed with upgrades
-    user_choice = input("Do you want to proceed with upgrades for all accounts? (yes/no): ").strip().lower()
-    
-    while True:
-        process_accounts(user_choice)  # Process all accounts
-        # You can add logic here if you need to break the loop or handle interruptions
-
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
